b9a6867b9e446936ed6749146c96b6ff8e1e1968
Melat t <melattesfaye16@gmail.com>
2024-04-22 21:26:43 +0300

    added testcases for youtube projects

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
+
+        System.setProperty("webdriver.edge.driver",
+                "C:\\Users\\OMEN 16\\Downloads\\edgedriver_win64\\msedgedriver.exe");
+
+        WebDriver driver = new EdgeDriver();
+
+        // System.out.println("Browser Initiated Successfully");
+
+        driver.get("https://www.facebook.com");
+
+        Thread.sleep(10000);
     }
 }
--- src/Google.java
+++ src/Google.java
@@ -2,34 +2,33 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class Google {
-   public static void main(String[] args) throws InterruptedException {
-        
+    public static void main(String[] args) throws InterruptedException {
 
-    System.setProperty("webdriver.chrome.driver", "C:\\Users\\OMEN 16\\Documents\\Shcool Stuff\\4th-1st Semister\\Software Testing\\Testings\\Test_1\\src\\drivers\\chromedriver.exe");
+        System.setProperty("webdriver.edge.driver",
+                "C:\\Users\\OMEN 16\\Downloads\\edgedriver_win64\\msedgedriver.exe");
 
-    WebDriver driver = new ChromeDriver();
+        WebDriver driver = new EdgeDriver();
 
-    System.out.println("Browser Initiated Successfully");     
-    
-    driver.get("https://www.facebook.com");
-    System.out.println("Website opened Successfully");
+        System.out.println("Browser Initiated Successfully");
 
-    String title = driver.getTitle();
-    String url = driver.getCurrentUrl();
+        driver.get("https://www.facebook.com");
+        // System.out.println("Website opened Successfully");
 
-    System.out.println("The title : "+title);
-    System.out.println("The url is "+url);
+        // String title = driver.getTitle();
+        // String url = driver.getCurrentUrl();
 
-    WebElement tf = driver.findElement(By.name("q"));
+        // System.out.println("The title : " + title);
+        // System.out.println("The url is " + url);
 
-    tf.sendKeys("What is selenium", Keys.ENTER);
+        // WebElement tf = driver.findElement(By.name("q"));
 
-    Thread.sleep(10000);
-    driver.quit();
+        // tf.sendKeys("What is selenium", Keys.ENTER);
 
+        // Thread.sleep(10000);
+        // driver.quit();
 
     }
 }
